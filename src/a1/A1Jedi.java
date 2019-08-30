d709c90ba83f0d4611a0521f2aa05d57a1a2d69f
emun99 <emun99@emun99-pc.wireless-1x.unc.edu>
2019-08-30 14:15:37 -0400

    Changed the placement of the array to have the count of items bought.

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -23,6 +23,7 @@ public class A1Jedi {
 		 int total_customers = scan.nextInt();
 		 String [] customer_name = new String[total_customers];
 		 int[] count_array = new int [total_products];
+		 
 		// int overall_count = 0;
 		 
 		 
@@ -31,20 +32,32 @@ public class A1Jedi {
 			customer_name[i] += " " + scan.next();
 			
 			int products_bought = scan.nextInt();
+			int count = 0;
 				
 			 for(int j=0; j<products_bought; j++) {
 					int quantity = scan.nextInt();
 					String food_name = scan.next();
-					int count = 0;
+					
 					
 					for(int k=0; k<total_products; k++) {
 						if(food_name.equals(food[k])){
 							count = count + quantity;
 							// overall_count = overall_count + count;
-							count_array[k] = count;
+							//count_array[k] = count;
 						}
 					}
 			 }
+			 count_array[i] = count;
+			 
+			 // DON'T make an array for each customer.
+			 // instead make a total count array near the end.
+			 // Like just make a count in the for loop and put that final count inside the final count array.
+			 
+			 /* Count how many of one item the customer bought & how many customers bought one item
+			  * int item_count = 0;
+			  * int customer_count = 0;
+			  */
+			
 		 }
 		
 		 
