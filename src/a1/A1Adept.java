b6a1bb39ab3c38037fe71a77e5cc3c0de19390d4
emun99 <emun99@emun99-pc.wireless-1x.unc.edu>
2019-08-30 12:37:55 -0400

    Erased comments and cleaned the code.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -3,8 +3,6 @@ package a1;
 import java.util.Scanner;
 
 public class A1Adept {
-	
-	// static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
@@ -54,20 +52,15 @@ public class A1Adept {
 				}
 				individual_cost[i] = overall_cost;
 			}
-			
-		//	individual_cost[i] = overall_cost;
-			
-			
-		
 		
 		
 		}
 		String max = max_value(customer_name, individual_cost);
 		String min = min_value(customer_name, individual_cost);
 		String average = average_value(individual_cost);
-		
-		// System.out.println(customer_name[i] + ": " + individual_cost[i]);	
+			
 		scan.close();
+		
 		System.out.println("Biggest: " + max);
 		System.out.println(" Smallest: " + min);
 		System.out.println(" Average: " + average);
@@ -115,7 +108,6 @@ public class A1Adept {
 		}
 
 		
-		
 		static String average_value(double[] vals) {
 			
 			double sum = 0;
@@ -127,119 +119,6 @@ public class A1Adept {
 			
 			return String.format("%.2f", sum/division);
 		}
-		
-	
-		
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	/*	double individual_cost = 0.0;
-		double total_cost = 0.0;
-		int total_customers = scan.nextInt();
-		 // int individual_bought = scan.nextInt();
-		
-		for(int j=0; j<total_customers; j++) {
-			// the outer loop will go through each individual item that the person bought
-			String [] names = new String [total_customers];
-			names[j] = scan.next();
-			
-			int individual_bought = scan.nextInt();
-			
-			for(int k=0; k<total_items_in_store;k++) {
-				
-				// this inner loop will calculate the cost of buying each item
-				int count_one_item = scan.nextInt();
-				
-				if (scan.next() == food[k]) {
-					individual_cost = count_one_item * prices[k];
-					total_cost += individual_cost;
-				}
-			}
-			
-		}
-		*/
-		
-		// System.out.println();
-		
-	//	scan.close();
-		
-		
-		/* static double most_spent() {
-			
-		}
-		
-		
-		*/
-		
-		
-	/* 1.Scanner first reads scan.next.Int() which is total # of items in store
-	 * Then reads word = scan.next();
-	 * Then reads price = scan.nextDouble();
-	 *  
-	 * for(int i=0; i<total_items_in_store; i++) {
-	 *  word = scan.next();
-	 *  price = scan.nextDouble();
-	 *  }
-	 *  // how do i make a new variable for each
-	 *  food item so that I can reference it later 
-	 *  when I am trying to add up the cost?
-	 *  
-	 *  ^Can I create an array to store these names
-	 *  of the food & their prices so I can later
-	 *  access their prices
-	 *  something like
-	 *  
-	 *  String [] all_food = [scan.nextInt()];
-	 *  
-	 *  if (all_food[i] == scan.next()) {
-	 *  cost = price_of_food * amount_of_one_food;
-	 *  }
-	 *  
-	 *  ^create another array just for prices?
-	 *  double [] all_food_prices = [total_items_in_store];
-	 *  
-	 *  
-	 *  2. Then it will read the # of customers
-	 *  int number_of_customers = scan.nextInt();
-	 *  
-	 *  for(int j=0; j<number_of_customers;j++) {
-	 *   if (all_food[i] == scan.next()) {
-	 *  cost = price_of_food * amount_of_one_food;
-	 *  }
-	 *  }
-	 *  
-	 *  
-	 *  nested for loops just for food name & food price?
-	 *  
-	 *  
-	 *  3. Need methods to get who spent the most, who
-	 *  spent the least, and the average money spent for
-	 *  all customers
-	 *  
-	 *  4. For each individual person, first go through
-	 *  first_name = scan.next();
-	 *  total_bought = scan.nextInt();
-	 *  
-	 *  while (total_bought > 0
-	 *  
-	 *  
-	 *  Do i need to make a variable for each food item?
-	 *  and save their prices so later in my for loop i can 
-	 *  write
-	 *  if (food name .equals (scan.next()) {
-	 * 	cost = food price * amount_of_one_food;
-	 *  }
-	 */
 		
 		
 	}
