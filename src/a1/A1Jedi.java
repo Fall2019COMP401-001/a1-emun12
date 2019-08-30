1e4cd11f92e3699f6ff51dbacb7cc2c9f626a0b5
emun99 <emun99@emun99-pc.wireless-1x.unc.edu>
2019-08-30 13:25:39 -0400

    Made a count variable to count how many of one item is bought.

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,46 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int total_products = scan.nextInt();
 		
+		String[] food = new String [total_products];
+		double [] prices = new double [total_products];
+		
+		 for(int i=0; i<total_products; i++) {
+			food[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		 // this loop puts the name of the food inside an array
+		 // this loop puts in the prices of the food inside an array
+		 
+		 int total_customers = scan.nextInt();
+		 String [] customer_name = new String[total_customers];
+		 int[] count_array = new int [total_products];
+		// int overall_count = 0;
+		 
+		 
+		 for(int i=0; i<total_customers;i++) {
+			customer_name[i] = scan.next();
+			customer_name[i] += " " + scan.next();
+			
+			int products_bought = scan.nextInt();
+				
+			 for(int j=0; j<products_bought; j++) {
+					int quantity = scan.nextInt();
+					String food_name = scan.next();
+					int count = 0;
+					
+					for(int k=0; k<total_products; k++) {
+						if(food_name.equals(food[k])){
+							count = count + quantity;
+							// overall_count = overall_count + count;
+							count_array[k] = count;
+						}
+					}
+			 }
+		 }
+		
+		 
+		 System.out.println(count_array[1]);
 	}
 }
